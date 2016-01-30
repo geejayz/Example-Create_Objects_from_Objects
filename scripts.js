8214cac5b18ad60438c219f4049123fe7274239e
geejayz <geejayz@users.noreply.github.com>
2016-01-30 21:43:19 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,8 +8,24 @@ var carObject = {
 
 
 var bmw = Object.create(carObject);
-bmw.make = "BMW";
+bmw.make = "bmw";
 bmw.model = "M5";
 bmw.year = 2011;
 
 console.log(bmw.depreciation());
+
+carObject.notes = "Leather Interior";  
+// This still gets copied over to any new objects created from this object
+console.log(bmw.notes);
+
+bmw.special = "4WD";
+//The following however will return an error
+console.log(carObject.special);
+
+
+for (var key in bmw) {
+	//Show everything that hasn't been inherited from the source
+	if(bmw.hasOwnProperty(key)) {
+		console.log(key + " : " + bmw[key]);
+	}
+}
